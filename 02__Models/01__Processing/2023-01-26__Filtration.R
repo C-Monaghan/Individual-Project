4ed8bb65ad477e7fe743cb40d144080008c2ec9d
C-Monaghan <cormacmonaghan@tutanota.com>
2023-01-28 12:46:26 +0000

    Messing around

--- 02__Models/01__Processing/2023-01-26__Filtration.R
+++ 02__Models/01__Processing/2023-01-26__Filtration.R
@@ -4,12 +4,25 @@ library(dplyr)
 
 path_data <- "./01__Data/HRS_2020/"
 
-# Reading in Section V of HRS 2020 Data
-demographics_r <- read_sav(file.path(path_data, "h20b_r.sav"))
-module_V_r <- read_sav(file.path(path_data, "h20v_r.sav"))
+# Tracker file containing information on each participant
+tracker <- read_sav("01__Data/Tracker.sav")
+
+# Reading in HRS 2020 Data
+demographics <- read_sav(file.path(path_data, "h20b_r.sav"))
+physical_health <- read_sav(file.path(path_data, "h20c_r.sav"))
+cognition <- read_sav(file.path(path_data, "h20d_r.sav"))
+
+module_V <- read_sav(file.path(path_data, "h20v_r.sav")) # Experimental Module with the procrastination data
 
 # Out of the 15723 participants in the data set only 1368 answered the procrastination scale
-module_v_r__procrastination <- module_V_r|>
+procrastination_participants <- module_V|>
   filter(!is.na(RV155))
 
-x <- inner_join(demographics_r, module_v_r__procrastination, by = c("HHID", "PN"))
+# Filtering down to ONLY those 1368 participants
+procrastination_participants_tracker <- inner_join(tracker, procrastination_participants, by = c("HHID", "PN"))
+procrastination_participants_demographics <- inner_join(demographics, procrastination_participants, by = c("HHID", "PN"))
+procrastination_participants_physical_health <- inner_join(physical_health, procrastination_participants, by = c("HHID", "PN"))
+procrastination_participants_cognition <- inner_join(cognition, procrastination_participants, by = c("HHID", "PN"))
+
+
+procrastination_participants_tracker$QWGTR
