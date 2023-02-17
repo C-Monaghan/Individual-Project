c17dfbf3c1301ec010bcf504036a2e63fb5037ac
C-Monaghan <cormacmonaghan@tutanota.com>
2023-02-17 17:13:22 +0000

    Reverse scoring certain variables

---
+++ 02__Models/01__Processing/2023-02-17__Reverse_scoring.R
@@ -0,0 +1,23 @@
+# Write description
+# -----------------------------------------------------------------------------
+rm(list=ls()) # Clearing work space
+
+path_data <- "./01__Data/02__Processed_data/"
+
+# Reading in data -------------------------------------------------------------
+hrs20_data <- readxl::read_xlsx(file.path(path_data, "HRS_2020_Data.xlsx"))
+
+# Calculating maximum value for personality
+max_value <- max(hrs20_data$Careless, na.rm = TRUE)
+
+# Creating columns with reverse scoring for certain variables
+hrs20_data <- hrs20_data |>
+  dplyr::mutate(Careless = max_value + 1 - Careless) |>
+  dplyr::mutate(Impulsive = max_value + 1 - Impulsive) |>
+  dplyr::mutate(Thrifty = max_value + 1 - Thrifty) |>
+  dplyr::mutate(Calm = max_value + 1 - Calm)
+
+# Exporting -------------------------------------------------------------------
+export_path <- "./01__Data/02__Processed_data/"
+
+writexl::write_xlsx(hrs20_data, path = file.path(export_path, "HRS_2020_Data.xlsx"))
