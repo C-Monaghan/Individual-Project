ba28999626e6478f2a1f484d84f4ae9054de9b67
C-Monaghan <cormacmonaghan@tutanota.com>
2023-03-06 11:22:27 +0000

    Repositioning nodes

--- 02__Models/02__DAG/1. DAG_generation.R
+++ 02__Models/02__DAG/1. DAG_generation.R
@@ -2,91 +2,90 @@
 # previously created using the online DAGitty software. This is done with the help of the ggdag package, which utlises
 # DAGitty to create and analyze structural causal models and plot them  using ggplot2 and ggraph in a consistent and easy manner
 # -----------------------------------------------------------------------------
-
+  
 library(ggplot2)
 library(ggdag)
-
+  
 # Creating DAG ----------------------------------------------------------------
 # Initially, the DAG was created in the online DAgitty software - the outputted code is then copied into R
 # ggdag is then used to tidy up the graph and add better structure
 # To better highlight the outcome variable, we use to dplyr to change the colour of the procrastination node
-
+  
 project_dag <- dagitty::dagitty('dag {
-bb="-3.061,-3.755,2.929,3.852"
-"HA" [pos="-2.559,2.697"]
-"LS" [pos="-0.369,3.515"]
-"Living status" [pos="2.552,2.845"]
-"MS" [pos="-0.303,-3.366"]
-"PA" [pos="-2.526,3.282"]
-"Self-Control" [pos="1.484,-2.783"]
-Age [exposure,pos="-2.865,0.303"]
-Anxiety [pos="0.736,2.834"]
-Conscientiousness [pos="-1.936,-3.046"]
-Depression [pos="0.713,1.301"]
-Education [pos="-0.640,-1.421"]
-Loneliness [pos="1.673,3.612"]
-Neuroticism [pos="-2.147,1.598"]
-Procrastination [outcome,pos="2.436,-0.068"]
-Retirement [pos="2.557,1.944"]
-Stress [pos="-1.987,-0.998"]
-"HA" -> "LS"
-"LS" -> Procrastination
-"Living status" -> Loneliness
-"MS" -> "LS"
-"MS" -> Depression
-"MS" -> Loneliness [pos="2.586,-0.439"]
-"MS" -> Procrastination [pos="1.094,-2.874"]
-"PA" -> "LS"
-"Self-Control" -> "LS" [pos="0.170,0.994"]
-"Self-Control" -> Procrastination [pos="2.204,-1.444"]
-Age -> "LS" [pos="-2.593,2.639"]
-Age -> Conscientiousness [pos="-2.421,-2.748"]
-Age -> Neuroticism
-Age -> Procrastination
-Anxiety -> "LS"
-Anxiety -> Loneliness
-Anxiety -> Procrastination [pos="2.210,1.849"]
-Conscientiousness -> "LS"
-Conscientiousness -> "Self-Control" [pos="-0.741,-3.202"]
-Conscientiousness -> Education
-Conscientiousness -> Procrastination
-Conscientiousness -> Stress [pos="-2.116,-1.415"]
-Depression -> "LS"
-Depression -> "Self-Control" [pos="1.612,-0.353"]
-Depression -> Procrastination
-Education -> "LS"
-Education -> Procrastination
-Loneliness -> "LS"
-Loneliness -> Depression [pos="1.796,2.056"]
-Loneliness -> Procrastination [pos="2.518,0.626"]
-Neuroticism -> "LS" [pos="-1.719,2.617"]
-Neuroticism -> "MS" [pos="-1.222,-1.329"]
-Neuroticism -> Anxiety [pos="-0.933,2.833"]
-Neuroticism -> Depression
-Neuroticism -> Loneliness [pos="0.748,1.621"]
-Neuroticism -> Procrastination
-Neuroticism -> Stress [pos="-2.225,-0.344"]
-Retirement -> "LS" [pos="1.444,3.200"]
-Retirement -> Procrastination [pos="2.728,0.882"]
-Stress -> "LS" [pos="-0.825,2.781"]
-Stress -> "Self-Control" [pos="0.183,-2.296"]
-Stress -> Procrastination}') %>%
+  bb="-3.061,-3.755,2.929,3.852"
+  "HA" [pos="-2.559,2.697"]
+  "LS" [pos="-0.369,3.515"]
+  "Living status" [pos="2.552,2.845"]
+  "MS" [pos="-0.303,-3.366"]
+  "PA" [pos="-2.526,3.282"]
+  "Self-Control" [pos="1.484,-2.783"]
+  Age [exposure,pos="-2.578,0.000"]
+  Anxiety [pos="0.736,2.525"]
+  Conscientiousness [pos="-1.936,-3.046"]
+  Depression [pos="0.713,1.301"]
+  Education [pos="-0.640,-1.234"]
+  Loneliness [pos="1.673,3.612"]
+  Neuroticism [pos="-2.147,1.675"]
+  Procrastination [outcome,pos="2.578,0.000"]
+  Retirement [pos="2.557,1.944"]
+  Stress [pos="-1.987,-0.998"]
+  "HA" -> "LS"
+  "LS" -> Procrastination
+  "Living status" -> Loneliness
+  "MS" -> "LS"
+  "MS" -> Depression
+  "MS" -> Loneliness [pos="2.586,-0.439"]
+  "MS" -> Procrastination [pos="1.094,-2.874"]
+  "PA" -> "LS"
+  "Self-Control" -> "LS" [pos="0.170,0.994"]
+  "Self-Control" -> Procrastination [pos="2.204,-1.444"]
+  Age -> "LS" [pos="-2.593,2.639"]
+  Age -> Conscientiousness [pos="-2.421,-2.748"]
+  Age -> Neuroticism
+  Age -> Procrastination
+  Anxiety -> "LS"
+  Anxiety -> Loneliness
+  Anxiety -> Procrastination [pos="2.210,1.849"]
+  Conscientiousness -> "LS"
+  Conscientiousness -> "Self-Control" [pos="-0.741,-3.202"]
+  Conscientiousness -> Education
+  Conscientiousness -> Procrastination
+  Conscientiousness -> Stress [pos="-2.116,-1.415"]
+  Depression -> "LS"
+  Depression -> "Self-Control" [pos="1.612,-0.353"]
+  Depression -> Procrastination
+  Education -> "LS"
+  Education -> Procrastination
+  Loneliness -> "LS"
+  Loneliness -> Depression [pos="1.796,2.056"]
+  Loneliness -> Procrastination [pos="2.518,0.626"]
+  Neuroticism -> "LS" [pos="-1.719,2.617"]
+  Neuroticism -> "MS" [pos="-1.222,-1.329"]
+  Neuroticism -> Anxiety [pos="-0.933,2.833"]
+  Neuroticism -> Depression
+  Neuroticism -> Loneliness [pos="0.748,1.621"]
+  Neuroticism -> Procrastination
+  Neuroticism -> Stress [pos="-2.225,-0.344"]
+  Retirement -> "LS" [pos="1.444,3.200"]
+  Retirement -> Procrastination [pos="2.728,0.882"]
+  Stress -> "LS" [pos="-0.825,2.781"]
+  Stress -> "Self-Control" [pos="0.183,-2.296"]
+  Stress -> Procrastination}') %>%
   tidy_dagitty() %>%
   dplyr::mutate(colour = ifelse(name == "Procrastination" | name == "Age", "Blue", "Red"))
-
+  
 # Plotting DAG ----------------------------------------------------------------
 dag_graph <- project_dag %>%
   ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_dag_point(aes(colour = colour), size =20) +  
-  geom_dag_text(size = 2.25, col = "black") +
-  geom_dag_edges_arc() +
+  geom_dag_point(aes(colour = colour), size = 18) +
+  geom_dag_text(size = 2, col = "black") +
+  geom_dag_edges() +
   theme_dag(legend.position = "none") +
   theme(panel.background = element_rect(fill = "white"), 
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
-
+  
 # Exporting -------------------------------------------------------------------
 export_path <- "./02__Models/02__DAG/"
-
-cowplot::save_plot(file.path(export_path, "results/DAG.png"), dag_graph, 
-                   base_height = 10)
+  
+cowplot::save_plot(file.path(export_path, "results/DAG.png"), dag_graph, base_height = 10)
