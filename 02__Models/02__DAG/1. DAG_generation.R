be31343555d00dbdf456169e51d849aa1bc6b899
C-Monaghan <cormacmonaghan@tutanota.com>
2023-03-08 16:48:25 +0000

    Adding mediation DAG

--- 02__Models/02__DAG/1. DAG_generation.R
+++ 02__Models/02__DAG/1. DAG_generation.R
@@ -2,7 +2,8 @@
 # previously created using the online DAGitty software. This is done with the help of the ggdag package, which utlises
 # DAGitty to create and analyze structural causal models and plot them  using ggplot2 and ggraph in a consistent and easy manner
 # -----------------------------------------------------------------------------
-  
+rm(list=ls()) # Clearing work space
+
 library(ggplot2)
 library(ggdag)
   
@@ -11,7 +12,7 @@ library(ggdag)
 # ggdag is then used to tidy up the graph and add better structure
 # To better highlight the outcome variable, we use to dplyr to change the colour of the procrastination node
   
-project_dag <- dagitty::dagitty('dag {
+project_dag_full <- dagitty::dagitty('dag {
   bb="-3.061,-3.755,2.929,3.852"
   "HA" [pos="-2.559,2.697"]
   "LS" [pos="-0.369,3.515"]
@@ -73,19 +74,54 @@ project_dag <- dagitty::dagitty('dag {
   Stress -> Procrastination}') %>%
   tidy_dagitty() %>%
   dplyr::mutate(colour = ifelse(name == "Procrastination" | name == "Age", "Blue", "Red"))
+
+# A shortened DAG is then created for the purpose of the parallel-process 
+# mediation analysis
+project_dag_short <- dagitty::dagitty('dag {
+  bb="0,0,1,1"
+  "Life Satisfaction" [pos="0.000,0.250"]
+  Age [exposure,pos="-2.500, 0.000"]
+  Conscientiousness [pos="0.000,0.500"]
+  Neuroticism [pos="0.000,0.750"]
+  Procrastination [outcome,pos="2.500,0.000"]
+  "Life Satisfaction" -> Procrastination
+  Age -> "Life Satisfaction"
+  Age -> Conscientiousness
+  Age -> Neuroticism
+  Age -> Procrastination
+  Conscientiousness -> Procrastination
+  Neuroticism -> Procrastination}') %>%
+  tidy_dagitty() %>%
+  dplyr::mutate(colour = ifelse(name == "Procrastination" | name == "Age", "Blue", "Red"))
   
 # Plotting DAG ----------------------------------------------------------------
-dag_graph <- project_dag %>%
+dag_graph_full <- project_dag_full %>%
   ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_dag_point(aes(colour = colour), size = 18) +
   geom_dag_text(size = 2, col = "black") +
   geom_dag_edges() +
   theme_dag(legend.position = "none") +
+  ggtitle("Predictors of Procrastination in Older Age Adults (DAG)") +
   theme(panel.background = element_rect(fill = "white"), 
         panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-  
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(vjust = -10, hjust = 0.5)) +
+  annotate("text", x = 0, y = -4, label = "Note: PA = Physical Activity | HA = Health Assessment | LS = Life Satisfaction | MS = Marital Status")
+
+dag_graph_short <- project_dag_short %>%
+  ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_point(aes(colour = colour), size = 28) +
+  geom_dag_text(size = 2, col = "black") +
+  geom_dag_edges() +
+  theme_dag(legend.position = "none") +
+  ggtitle("Mediators of Procrastination in Older Age Adults (DAG)") +
+  theme(panel.background = element_rect(fill = "white"), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(vjust = -10, hjust = 0.5))
 # Exporting -------------------------------------------------------------------
 export_path <- "./02__Models/02__DAG/"
   
-cowplot::save_plot(file.path(export_path, "results/DAG.png"), dag_graph, base_height = 10)
+cowplot::save_plot(file.path(export_path, "results/01_DAG_full.png"), dag_graph_full, base_height = 10)
+cowplot::save_plot(file.path(export_path, "results/02_DAG_short.png"), dag_graph_short, base_height = 10)
+
